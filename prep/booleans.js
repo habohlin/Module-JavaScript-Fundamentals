22456598ae288887314cb6e2cbe456056ad19d1a
Hanna Bohlin <ha.bohlin@gmail.com>
2026-09-15 15:08:59 +0100

    Saving changes before creating new branch

---
+++ prep/booleans.js
@@ -0,0 +1,6 @@
+console.log("hello" === "hello");
+console.log("CYF" === "cyf");
+const homeTown = "Newcastle";
+console.log(homeTown === "Liverpool");
+console.log(42 === 42);
+console.log(42 == "42");
